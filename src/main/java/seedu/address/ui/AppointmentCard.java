8322c10b5f387a5bcd610b5c914af1d03c101a70
joshenx <e0525998@u.nus.edu>
2021-10-19 04:40:42 +0800

    Add UI functionality for Appointments and Archived Appointments

--- src/main/java/seedu/address/ui/AppointmentCard.java
+++ src/main/java/seedu/address/ui/AppointmentCard.java
@@ -1,20 +1,21 @@
 package seedu.address.ui;
 
-import java.util.Comparator;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import seedu.address.model.appointment.Appointment;
 import seedu.address.model.person.Patient;
 
-/**
- * An UI component that displays information of a {@code Person}.
- */
-public class PersonCard extends UiPart<Region> {
+import java.util.Comparator;
 
-    private static final String FXML = "PersonListCard.fxml";
+/**
+ * An UI component that displays information of an {@code Appointment}.
+ */
+public class AppointmentCard extends UiPart<Region> {
+
+    private static final String FXML = "AppointmentListCard.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -24,10 +25,10 @@ public class PersonCard extends UiPart<Region> {
      * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
      */
 
-    public final Patient patient;
+    public final Appointment appointment;
 
     @FXML
-    private HBox cardPane;
+    private HBox apptCardPane;
     @FXML
     private Label name;
     @FXML
@@ -41,14 +42,17 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label medicalHistory;
     @FXML
+    private Label date;
+    @FXML
     private FlowPane tags;
 
     /**
-     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     * Creates a {@code AppointmentCode} with the given {@code Appointment} and index to display.
      */
-    public PersonCard(Patient patient, int displayedIndex) {
+    public AppointmentCard(Appointment appointment, int displayedIndex) {
         super(FXML);
-        this.patient = patient;
+        this.appointment = appointment;
+        Patient patient = appointment.getPatient();
         id.setText(displayedIndex + ". ");
         name.setText(patient.getName().fullName);
         phone.setText("\uD83D\uDCDE\t" + patient.getPhone().value);
@@ -58,6 +62,7 @@ public class PersonCard extends UiPart<Region> {
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
         medicalHistory.setText("\uD83C\uDFE5\t" + patient.getMedicalHistory());
+        date.setText("\uD83D\uDCC5\t" + appointment.getDatetime());
     }
 
     @Override
@@ -68,13 +73,13 @@ public class PersonCard extends UiPart<Region> {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof PersonCard)) {
+        if (!(other instanceof AppointmentCard)) {
             return false;
         }
 
         // state check
-        PersonCard card = (PersonCard) other;
+        AppointmentCard card = (AppointmentCard) other;
         return id.getText().equals(card.id.getText())
-                && patient.equals(card.patient);
+                && appointment.equals(card.appointment);
     }
 }
