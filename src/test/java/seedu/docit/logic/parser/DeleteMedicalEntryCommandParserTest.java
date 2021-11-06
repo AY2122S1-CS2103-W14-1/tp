7beae690b6f50fbc1d3007b5c08c85387d9d1739
Didymus <e0544195@u.nus.edu>
2021-11-06 19:30:38 +0800

    Add one failure test case for DeleteMedicalEntryCommandParserTest

--- src/test/java/seedu/docit/logic/parser/DeleteMedicalEntryCommandParserTest.java
+++ src/test/java/seedu/docit/logic/parser/DeleteMedicalEntryCommandParserTest.java
@@ -1,18 +1,30 @@
 package seedu.docit.logic.parser;
 
+import static seedu.docit.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.docit.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
+import static seedu.docit.logic.parser.PatientCommandParserTestUtil.assertParseFailure;
 import static seedu.docit.logic.parser.PatientCommandParserTestUtil.assertParseSuccess;
 import static seedu.docit.testutil.TypicalIndexes.INDEX_FIRST_PATIENT;
+
 import org.junit.jupiter.api.Test;
+
+import seedu.docit.commons.core.index.Index;
 import seedu.docit.logic.commands.DeleteMedicalEntryCommand;
-import seedu.docit.model.patient.MedicalHistory;
 
 public class DeleteMedicalEntryCommandParserTest {
     private DeleteMedicalEntryCommandParser parser = new DeleteMedicalEntryCommandParser();
 
     @Test
-    public void parsePatientCommand_validMedicalEntry_successFul() {
+    public void parsePatientCommand_validDeletion_successFul() {
         assertParseSuccess(parser, PREAMBLE_WHITESPACE + "1 i/1",
             new DeleteMedicalEntryCommand(INDEX_FIRST_PATIENT, INDEX_FIRST_PATIENT));
+    }
+
+    @Test
+    public void parsePatientCommand_invalidPatientIndex_successFul() {
+        Index invalidPatientIndex = Index.fromOneBased(Integer.MAX_VALUE);
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            DeleteMedicalEntryCommand.MESSAGE_USAGE);
+        assertParseFailure(parser, invalidPatientIndex.getOneBased() + "i/1", expectedMessage);
     }
 }
