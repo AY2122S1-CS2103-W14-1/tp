cb52383fdb6f7fdf3a2603f5050618757020eb8c
Gabriel <77230723+gycgabriel@users.noreply.github.com>
2021-11-07 19:04:32 +0800

    Create test for JsonSerializableAppointmentBook

--- src/test/java/seedu/docit/storage/JsonSerializableAppointmentBookTest.java
+++ src/test/java/seedu/docit/storage/JsonSerializableAppointmentBookTest.java
@@ -4,14 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.docit.testutil.Assert.assertThrows;
 import static seedu.docit.testutil.TypicalPatients.getTypicalAddressBook;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.docit.commons.exceptions.IllegalValueException;
-import seedu.docit.commons.util.FileUtil;
 import seedu.docit.commons.util.JsonUtil;
 import seedu.docit.model.AppointmentBook;
 import seedu.docit.testutil.TypicalAppointments;
@@ -22,15 +20,6 @@ public class JsonSerializableAppointmentBookTest {
     private static final Path TYPICAL_APPOINTMENTS_FILE = TEST_DATA_FOLDER.resolve("typicalAppointmentsAppointmentBook.json");
     private static final Path INVALID_APPOINTMENT_FILE = TEST_DATA_FOLDER.resolve("invalidAppointmentAppointmentBook.json");
     private static final Path DUPLICATE_APPOINTMENT_FILE = TEST_DATA_FOLDER.resolve("duplicateAppointmentAppointmentBook.json");
-
-    @Test
-    public void hehee() throws IOException {
-        AppointmentBook typicalAppointmentsAppointmentBook = TypicalAppointments.getTypicalAppointmentBook();
-        FileUtil.createIfMissing(TYPICAL_APPOINTMENTS_FILE);
-        JsonUtil.saveJsonFile(new JsonSerializableAppointmentBook(typicalAppointmentsAppointmentBook, getTypicalAddressBook()),
-            TYPICAL_APPOINTMENTS_FILE);
-        assert true;
-    }
 
     @Test
     public void toModelType_typicalAppointmentsFile_success() throws Exception {
